976c9431524be2fd0f65006dfb16e9806bbe94d7
Madhumitha <madhumithavenu03@gmail.com>
2023-10-14 12:10:15 +0530

    Adding function for gameinit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
 const inputs = document.querySelector(".inputs"),
-resetBtn = document.querySelector(".reset-btn");
+    resetBtn = document.querySelector(".reset-btn"),
+    hint = document.querySelector(".hint span"),
+    typingInput = document.querySelector(".typing-input");
 
-function randomWord(){
+
+function randomWord() {
     //getting random object from list
-    let ranObj = wordList[Math.floor(Math.random()* wordList.length)];
+    let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
     let word = ranObj.word; //getting word of random object
     console.log(word);
 
+    hint.innerText = ranObj.hint;
+
     let html = "";
     for (let i = 0; i < word.length; i++) {
-        html += `<input type="text" disabled>`        
+        html += `<input type="text" disabled>`
     }
     inputs.innerHTML = html;
 }
 randomWord();
 
-resetBtn.addEventListener("click",randomWord);
+function initGame(e){
+    let key = e.target.value;
+    console.log(key);
+}
+
+resetBtn.addEventListener("click", randomWord);
+resetBtn.addEventListener("input", initGame);
+resetBtn.addEventListener("keydown", () => typingInput.focus());

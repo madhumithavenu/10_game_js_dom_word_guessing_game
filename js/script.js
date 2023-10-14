1cf6f81bf19b05681b26765c7b82aab971c1efb0
Madhumitha <madhumithavenu03@gmail.com>
2023-10-14 11:58:57 +0530

    Adding function to reset button

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
+const inputs = document.querySelector(".inputs"),
+resetBtn = document.querySelector(".reset-btn");
+
 function randomWord(){
     //getting random object from list
-    let ranObj = wordList[Math.floor(Math.random()* wordList.Length)];
+    let ranObj = wordList[Math.floor(Math.random()* wordList.length)];
     let word = ranObj.word; //getting word of random object
     console.log(word);
 
     let html = "";
     for (let i = 0; i < word.length; i++) {
-        html = `<input type="text" disabled>`
-        
+        html += `<input type="text" disabled>`        
     }
+    inputs.innerHTML = html;
 }
+randomWord();
+
+resetBtn.addEventListener("click",randomWord);
